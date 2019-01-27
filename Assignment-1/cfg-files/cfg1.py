0d99c6735dba5a40691c78568f0aeec8d4d7c234
garsin01 <40858250+garsin01@users.noreply.github.com>
2019-01-27 21:12:11 +0530

    Configuration files

---
+++ Assignment-1/cfg-files/cfg1.py
@@ -0,0 +1,26 @@
+from tokens import *
+from reserved_words import *
+
+colour = {}
+for d in data_types:
+	colour[reserved[d]] = 'Tomato'
+for k in keywords:
+	colour[reserved[k]] = 'Violet'
+for n in named_tokens:
+	colour[n] = 'White'
+for c in characters:
+	colour[c] = 'Yellow'
+for b in brackets:
+	colour[b] = 'SlateBlue'
+for m in comp:
+	colour[m] = 'Pink'
+for m in operations:
+	colour[m] = 'Pink'
+for m in logical_operations:
+	colour[m] = 'Pink'
+for m in assignment_symbols:
+	colour[m] = 'Pink'
+for m in end_of_lines:
+	colour[m] = 'Pink'
+for m in miscellaneous:
+	colour[m] = 'Pink'
